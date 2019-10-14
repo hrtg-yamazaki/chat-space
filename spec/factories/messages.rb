6752404feae39b1bce68b8f6f9df7076bfaa170c
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2019-10-14 14:21:14 +0900

    prep for rspec with devise
    コントローラのテストに向けて、deviseをrspecで使用するための準備を行いました。その他軽微な修正。

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :message do
-    body   {Faker::Lolem.sentence}
-    image  {File.open("#{Rails.root}/pubric/images/test_image.png")}
+    body   {Faker::Lorem.sentence}
+    image  {File.open("#{Rails.root}/public/images/test_image.png")}
     user   
     group  
   end
