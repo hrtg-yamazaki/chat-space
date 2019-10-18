438c4fdf215dafae17ef78c4826e3f8811efd6e5
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2019-10-18 19:47:15 +0900

    reflect for view

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,8 +1,24 @@
 $(function() {
-  console.log("ok");
+    function buildHTML(message){
+    var html = `<div class="message">
+                <div class="message__info">
+                  <p class="message__info__user">
+                    ${message.user_name}
+                  </p>
+                  <p class="message__info__timestamp">
+                    ${message.timestamp}
+                  </p>
+                </div>
+                <div class="message__text">
+                  ${message.body}
+                </div>
+                <img src='${ message.image ? message.image : "" }'>
+              </div>`
+    return html;
+  }
   
-
-  $("#new_message").on("submit", function(e){
+  
+  $("#new_message").on("submit", function(e) {
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr("action");
@@ -10,9 +26,21 @@ $(function() {
       url: url,
       type: "POST",
       data: formData,
-      datatype: "json",
+      dataType: "json",
       processData: false,
       contentType: false
-    });
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $(".messages").append(html);
+      $(".new-post__box__text").val("");
+      $(".new-post__box__image__file").val("");
+    })
+    .fail(function() {
+      alert("メッセージを入力してください");
+    })
   });
 });
+
+
+
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,3 +1,4 @@
 json.body @message.body
-json.image @message.image
-json.user_id @message.user_id
+json.image @message.image.url if @message.image.present?  
+json.user_name @message.user.name
+json.timestamp @message.created_at.strftime("%Y/%m/%d %H:%M")
