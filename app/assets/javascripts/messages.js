fd8d5fdecf27c0b88161c80f8f964aeb4f66ea2b
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2019-10-18 16:27:03 +0900

    setting ajax

---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,18 @@
+$(function() {
+  console.log("ok");
+  
+
+  $("#new_message").on("submit", function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr("action");
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      datatype: "json",
+      processData: false,
+      contentType: false
+    });
+  });
+});
